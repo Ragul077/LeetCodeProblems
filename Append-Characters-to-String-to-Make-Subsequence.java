4de1a19bce3c633d325795d16b888929a89b9532
Ragul077 <ragulmanickam9092@gmail.com>
2025-03-12 10:23:35 +0530

    [2486] [Time Beats: 96.19%] [Memory Beats: 25.99%] - LeetPush

---
+++ Append-Characters-to-String-to-Make-Subsequence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int appendCharacters(String s, String t) {
+        int i=0,j=0;
+        int sl = s.length();
+        int tl = t.length();
+        while(i<sl && j<tl){
+            if(s.charAt(i) == t.charAt(j)){
+                j++;
+            }
+            i++;
+        }
+        return tl-j;
+    }
+}
