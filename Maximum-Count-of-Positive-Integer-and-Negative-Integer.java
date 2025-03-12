c52603839770e94f9282709fe98a8abeca05cf4c
Ragul077 <ragulmanickam9092@gmail.com>
2025-03-12 09:09:37 +0530

    [2529] [Time Beats: 100.00%] [Memory Beats: 8.64%] - LeetPush

---
+++ Maximum-Count-of-Positive-Integer-and-Negative-Integer.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximumCount(int[] nums) {
+        int p=0,n=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>0)
+            {
+               p++;
+            }
+            if(nums[i]<0)
+            {
+                n++;
+            }
+        }
+        if(p>n)
+        {
+            return p;
+        }
+        return n;
+    }
+}
